50de0f7ba57f3dd116f25107f23f63e6a2f67596
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-02-24 14:21:51 +0100

    Add color theme

---
+++ my-app/constants/ThemeColors.js
@@ -0,0 +1,34 @@
+export const ThemeColors = {
+    primary: "#D9D9D9",
+    secondary: "#1113A6",
+    accent: "#00a1e6",
+
+    danger: "#b41c2b",
+    success: "#009f42",
+    warning: "#f0ad4e",
+    info: "#388cfa",
+
+    light: {
+        background: "#c6c6c6",
+        navBackground: "D9D9D9",
+        surface: "",
+        card: "",
+        text: "#0b0a0a",
+        textTitle: "#0b0a0a",
+        textMuted: "#0b0a0a",
+        icon: "",
+        iconFocused: "",
+    },
+
+    dark: {
+        background: "#5e5e5e",
+        navBackground: "D9D9D9",
+        surface: "",
+        card: "",
+        text: "#0b0a0a",
+        textTitle: "#0b0a0a",
+        textMuted: "#0b0a0a",
+        icon: "",
+        iconFocused: "",
+    }
+}
